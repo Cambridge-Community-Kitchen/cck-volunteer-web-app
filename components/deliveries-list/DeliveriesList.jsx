3fda4b31127e49fa2b17c19573b219ad887b7345
Daniel Perrett <perrettdl@googlemail.com>
2023-10-01 17:07:53 +0100

    Replace tabs with spaces in DeliveriesList.jsx

--- components/deliveries-list/DeliveriesList.jsx
+++ components/deliveries-list/DeliveriesList.jsx
@@ -14,11 +14,11 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 dayjs.extend(customParseFormat);
 
 const DeliveriesList = ({ date, id_ref, passcode, mode, basePath }) => {
-	const [displayDish, setDisplayDish] = useState(false);
-	const [isLoading, setIsLoading] = useState(null);
-	const [routeData, setRouteData] = useState();
+  const [displayDish, setDisplayDish] = useState(false);
+  const [isLoading, setIsLoading] = useState(null);
+  const [routeData, setRouteData] = useState();
 
-	const dishes = [
+  const dishes = [
     {
       title: 'Main dish',
       info: routeData?.event?.addl_info?.dishOfTheDay,
@@ -29,65 +29,65 @@ const DeliveriesList = ({ date, id_ref, passcode, mode, basePath }) => {
     }
   ].filter(dish => dish.info);
 
-	const router = useRouter();
+  const router = useRouter();
 
-	useEffect(() => {
-		setIsLoading(true);
-		getRouteData({basePath, date, ref:id_ref, passcode, mode}).then((response) => {
-			response.json().then((data) => {
-				setRouteData(data);
-				// Temporarily hardcoded to true; previous logic said only show dish if it's within the last 3 days
-				setDisplayDish(true);
-				setIsLoading(false);
-			});
-		});
-	}, [basePath, date, id_ref, passcode, mode]);
+  useEffect(() => {
+    setIsLoading(true);
+    getRouteData({basePath, date, ref:id_ref, passcode, mode}).then((response) => {
+      response.json().then((data) => {
+        setRouteData(data);
+        // Temporarily hardcoded to true; previous logic said only show dish if it's within the last 3 days
+        setDisplayDish(true);
+        setIsLoading(false);
+      });
+    });
+  }, [basePath, date, id_ref, passcode, mode]);
 
-	const updateItemCompletion = useCallback(
-		(id, value) => {
-			const routeDataCopy = JSON.parse(JSON.stringify(routeData));
-			routeDataCopy.deliveries[id]['completed'] = value;
-			setRouteData(routeDataCopy);
-		},
-		[routeData],
-	);
+  const updateItemCompletion = useCallback(
+    (id, value) => {
+      const routeDataCopy = JSON.parse(JSON.stringify(routeData));
+      routeDataCopy.deliveries[id]['completed'] = value;
+      setRouteData(routeDataCopy);
+    },
+    [routeData],
+  );
 
-	const markItemComplete = useCallback(
-		(id) => updateItemCompletion(id, true),
-		[updateItemCompletion],
-	);
+  const markItemComplete = useCallback(
+    (id) => updateItemCompletion(id, true),
+    [updateItemCompletion],
+  );
 
-	const unmarkItemComplete = useCallback(
-		(id) => updateItemCompletion(id, false),
-		[updateItemCompletion],
-	);
+  const unmarkItemComplete = useCallback(
+    (id) => updateItemCompletion(id, false),
+    [updateItemCompletion],
+  );
 
-	if (!routeData || isLoading) return <LoadingSpinner />;
+  if (!routeData || isLoading) return <LoadingSpinner />;
 
-	const region = routeData.name;
+  const region = routeData.name;
 
-	if (routeData.result) {
-		return <div>{routeData.result}</div>;
-	}
+  if (routeData.result) {
+    return <div>{routeData.result}</div>;
+  }
 
-	const rDate = new Date(routeData.event.start_date);
-	const formattedDate = ((rDate.getDate() > 9) ? rDate.getDate() : ('0' + rDate.getDate())) + '/' + ((rDate.getMonth() > 8) ? (rDate.getMonth() + 1) : ('0' + (rDate.getMonth() + 1))) + '/' + rDate.getFullYear();
+  const rDate = new Date(routeData.event.start_date);
+  const formattedDate = ((rDate.getDate() > 9) ? rDate.getDate() : ('0' + rDate.getDate())) + '/' + ((rDate.getMonth() > 8) ? (rDate.getMonth() + 1) : ('0' + (rDate.getMonth() + 1))) + '/' + rDate.getFullYear();
 
-	return (
-		<div className={styles.root}>
-			<Box display="flex" justifyContent="space-between">
-				<Box display="flex" ml={2} mr={2}>
-					<Text>Deliveries for </Text>
-					<Text fontWeight={700} ml={1}>
-					{formattedDate}
-					</Text>
-					<Text>&nbsp;in </Text>
-					<Text fontWeight={700} ml={1}>
-						{region}
-					</Text>
-				</Box>
-			</Box>
-			{displayDish && dishes.length && dishes.map(
+  return (
+    <div className={styles.root}>
+      <Box display="flex" justifyContent="space-between">
+        <Box display="flex" ml={2} mr={2}>
+          <Text>Deliveries for </Text>
+          <Text fontWeight={700} ml={1}>
+          {formattedDate}
+          </Text>
+          <Text>&nbsp;in </Text>
+          <Text fontWeight={700} ml={1}>
+            {region}
+          </Text>
+        </Box>
+      </Box>
+      {displayDish && dishes.length && dishes.map(
         (dish) => (
           <Dish
             dish_info={dish.info}
@@ -95,53 +95,53 @@ const DeliveriesList = ({ date, id_ref, passcode, mode, basePath }) => {
             key={dish.title}
           />
         )
-			)}
-			<Flex direction="row">
-			<Button
-				margin="auto"
-				as="a"
-				onClick={() => {
-					router.push({
-						pathname: '/api/cck/route.gpx',
-						query: {
-							date: date,
-							ref: id_ref,
-							passcode: passcode,
-							mode: mode
-						}
-					});
-				}}
-				rightIcon={<ArrowForwardIcon />}
-				colorScheme="blue"
-				target="_blank"
-			>
-				Download route as GPX
-			</Button></Flex>
-			<ul className={styles.list}>
-				{routeData.deliveries.map((item, index) => {
-					const portions = item.portions;
+      )}
+      <Flex direction="row">
+      <Button
+        margin="auto"
+        as="a"
+        onClick={() => {
+          router.push({
+            pathname: '/api/cck/route.gpx',
+            query: {
+              date: date,
+              ref: id_ref,
+              passcode: passcode,
+              mode: mode
+            }
+          });
+        }}
+        rightIcon={<ArrowForwardIcon />}
+        colorScheme="blue"
+        target="_blank"
+      >
+        Download route as GPX
+      </Button></Flex>
+      <ul className={styles.list}>
+        {routeData.deliveries.map((item, index) => {
+          const portions = item.portions;
 
-					if (item.plus_code.length <= 13 && item.plus_code.includes("+")) { //then it's a PlusCode
-						if (item.plus_code.indexOf("+")==4) {
-							item.plus_code = "9f42"+item.plus_code;
-						}
-					}
-					item.plusCode = item.plus_code;
-					item.whenNotHome = item.when_not_home;
-					return (
-						<Item
-							data={item}
-							key={index.toString()}
-							markComplete={() => markItemComplete(index)}
-							portions={portions.toString()}
-							unmarkComplete={() => unmarkItemComplete(index)}
-						/>
-					);
-				})}
-			</ul>
-			<BackToLockon />
-		</div>
-	);
+          if (item.plus_code.length <= 13 && item.plus_code.includes("+")) { //then it's a PlusCode
+            if (item.plus_code.indexOf("+")==4) {
+              item.plus_code = "9f42"+item.plus_code;
+            }
+          }
+          item.plusCode = item.plus_code;
+          item.whenNotHome = item.when_not_home;
+          return (
+            <Item
+              data={item}
+              key={index.toString()}
+              markComplete={() => markItemComplete(index)}
+              portions={portions.toString()}
+              unmarkComplete={() => unmarkItemComplete(index)}
+            />
+          );
+        })}
+      </ul>
+      <BackToLockon />
+    </div>
+  );
 };
 
 export default DeliveriesList;

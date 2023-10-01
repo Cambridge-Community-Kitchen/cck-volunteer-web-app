0a5f739e37fee584b19759e14ed410a4357c7243
Jo <31373245+jamdelion@users.noreply.github.com>
2023-10-01 14:35:00 +0100

    Display demo data if demo in route path (#23)

---
+++ pages/api/cck/demoRouteData.ts
@@ -0,0 +1,75 @@
+type Delivery = {
+  portions: number;
+  name: string;
+  phone?: string;
+  address: string;
+  plus_code: string;
+  allergies?: string;
+  notes?: string;
+  when_not_home?: string;
+};
+
+type RouteData = {
+  deliveries: Delivery[];
+  route_delivery: string;
+  name: string;
+  event: { start_date: Date | string };
+};
+
+export const demoRouteData: RouteData = {
+  deliveries: [
+    {
+      portions: 1,
+      name: "Sarina",
+      phone: "01234 567890",
+      address: "9 Wiegman Road, Cambridge CB9 9SW",
+      plus_code: "6574+2F",
+      allergies: "",
+      notes: "",
+      when_not_home: "Don't leave food",
+    },
+    {
+      portions: 9,
+      name: "Lucy Bronze",
+      phone: "01234 567890",
+      address: "10 Lioness Road, Cambridge CB1 1LB",
+      plus_code: "647F+26",
+      allergies: "No spicy food",
+      notes: "",
+      when_not_home: "Leave at front door",
+    },
+    {
+      portions: 1,
+      name: "Millie Bright",
+      phone: "01234 567890",
+      address: "2b England Way, Cambridge CB7 1MB",
+      plus_code: "643M+XP",
+      allergies: "",
+      notes: "Please be patient when ringing doorbell",
+      when_not_home: "Do not leave if not in",
+    },
+    {
+      portions: 2,
+      name: "Georgia Stanway",
+      phone: "07701234567",
+      address: "212, Champion Avenue, Cambridge CB7 1GS",
+      plus_code: "642Q+29",
+      allergies: "",
+      notes: "",
+      when_not_home: "",
+    },
+    {
+      portions: 3,
+      name: "Mary Earps",
+      phone: "07701234567",
+      address: "1 Goal Close, Cambridge CB1 1ME",
+      plus_code: "642F+MX",
+      allergies: "Gluten intolerant",
+      notes: "",
+      when_not_home: "",
+    },
+  ],
+  route_delivery: "route_delivery",
+  name: "Arbury (DEMO)",
+  event: { start_date: new Date() },
+};
--- pages/api/cck/route.ts
+++ pages/api/cck/route.ts
@@ -1,121 +1,130 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/components/db-connection-prisma';
-import { daysSince, parseDashedDate } from '@/components/api-helpers';
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/components/db-connection-prisma";
+import { daysSince, parseDashedDate } from "@/components/api-helpers";
+import { demoRouteData } from "./demoRouteData";
 
 /**
  * Gets route information
- * 
+ *
  * @param {NextApiRequest} req The Next.js API request
  * @param {NextApiResponse} res The Next.js API response
  */
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    
-    // To find route info, we need to know the date (e.g., 03-02-2022) and the route (e.g., arbury).
-    // Everything else can be calculated
-    // this is a cck-specific endpoint.  we know the organization is 'cck'
-    // this is a delivery-route-specific endpoint.  we know the event category is 'meal-prep-delivery' and that the role is 'delivery'
-    // we know, then, that the position (and route) ref is meal-prep-delivery-<<DD-MM_YYYY>>-delivery-<<route ref>> where org = 'cck'
-    
-    // The combination of cck, event category, and date should be unique for an event. If the database says that we're
-    // doing multiple meal preps and deliveries in one day, we messed up.
-     
-    // Do we need to include event, role, or position information? Probably in the future, but not right now.
-    // All that matters right now is that we can return route details based on date and route name
-    
-    // Example path: /route?date=03-02-2022&ref=arbury&passcode=ZHZW
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // To find route info, we need to know the date (e.g., 03-02-2022) and the route (e.g., arbury).
+  // Everything else can be calculated
+  // this is a cck-specific endpoint.  we know the organization is 'cck'
+  // this is a delivery-route-specific endpoint.  we know the event category is 'meal-prep-delivery' and that the role is 'delivery'
+  // we know, then, that the position (and route) ref is meal-prep-delivery-<<DD-MM_YYYY>>-delivery-<<route ref>> where org = 'cck'
 
-    const query = req.query;
-    const { date, ref, passcode } = query;
+  // The combination of cck, event category, and date should be unique for an event. If the database says that we're
+  // doing multiple meal preps and deliveries in one day, we messed up.
 
-    let parsedDate: Date;
-    try {
-        parsedDate = parseDashedDate(date as string);
-    } catch(error) {
-        res.status(400).json({ result: "invalid date" });
-        return;
-    }
-    const daysDiff = daysSince(parsedDate);
-    
-    // Do not return route data if the delivery date is more than a day ago
-    if(daysDiff > 1) {
-        res.status(404).json({ result: "route not found" });
-        return;
-    } else if(passcode) {
-        const routeRef = `meal-prep-delivery-${date}-delivery-${ref}`;
+  // Do we need to include event, role, or position information? Probably in the future, but not right now.
+  // All that matters right now is that we can return route details based on date and route name
 
-        const gottenRoute = await prisma.route.findFirst({
-            where: {
-                id_ref: routeRef,
+  // Example path: /route?date=03-02-2022&ref=arbury&passcode=ZHZW
+
+  const query = req.query;
+  const { date, ref, passcode } = query;
+
+  //   render demo data if the path is like /route?date=03-02-2022&ref=demo&passcode=ZHZW
+  if (ref === "demo") {
+    res.status(200).json(demoRouteData);
+  }
+  let parsedDate: Date;
+  try {
+    parsedDate = parseDashedDate(date as string);
+  } catch (error) {
+    res.status(400).json({ result: "invalid date" });
+    return;
+  }
+  const daysDiff = daysSince(parsedDate);
+
+  // Do not return route data if the delivery date is more than a day ago
+  if (daysDiff > 1) {
+    res.status(404).json({ result: "route not found" });
+    return;
+  } else if (passcode) {
+    const routeRef = `meal-prep-delivery-${date}-delivery-${ref}`;
+
+    const gottenRoute = await prisma.route.findFirst({
+      where: {
+        id_ref: routeRef,
+      },
+      include: {
+        route_delivery: {
+          orderBy: {
+            sequence: "asc",
+          },
+        },
+        event_position: {
+          include: {
+            event: {
+              include: {
+                event_category: true,
+              },
             },
-            include: {
-                route_delivery: {
-                    orderBy: {
-                        sequence: 'asc',
-                    }
-                },
-                event_position: {
-                    include: {
-                        event: {
-                            include: {
-                                event_category: true
-                            }
-                        }
-                    }
-                },
-            },
-        });
+          },
+        },
+      },
+    });
 
-        if(!gottenRoute) {
-            res.status(404).json({ result: "route not found" });
-            return;
-        }
-
-        if(gottenRoute.passcode !== passcode) {
-            res.status(403).json({ result: "Passcode is invalid" });
-            return;
-        }
-
-        cleanupRoute(gottenRoute);
-        
-        res.status(200).json(gottenRoute);
-        return;
-    } else {
-        res.status(403).json({ result: "For now, you MUST provide a passcode to access a route." });
-        return;
+    if (!gottenRoute) {
+      res.status(404).json({ result: "route not found" });
+      return;
     }
+
+    if (gottenRoute.passcode !== passcode) {
+      res.status(403).json({ result: "Passcode is invalid" });
+      return;
+    }
+
+    cleanupRoute(gottenRoute);
+
+    res.status(200).json(gottenRoute);
+    return;
+  } else {
+    res.status(403).json({
+      result: "For now, you MUST provide a passcode to access a route.",
+    });
+    return;
+  }
 }
 
 /**
  * Removes unneeded fields and changes field names to make the response more user-friendly
  */
 function cleanupRoute(gottenRoute) {
-    gottenRoute['deliveries'] = gottenRoute.route_delivery.map( route => {
-        delete route.sequence;
-        delete route.id;
-        delete route.id_route;
-        return route;
-    });
-    
-    delete gottenRoute.route_delivery;
-    delete gottenRoute.id;
-    delete gottenRoute.id_ref;
-    delete gottenRoute.id_event_position;
-    delete gottenRoute.passcode;
+  gottenRoute["deliveries"] = gottenRoute.route_delivery.map((route) => {
+    delete route.sequence;
+    delete route.id;
+    delete route.id_route;
+    return route;
+  });
 
-    const eventCategory = gottenRoute.event_position.event.event_category;
-    delete eventCategory.id;
-    delete eventCategory.id_organization;
-    delete eventCategory.id_ref;
+  delete gottenRoute.route_delivery;
+  delete gottenRoute.id;
+  delete gottenRoute.id_ref;
+  delete gottenRoute.id_event_position;
+  delete gottenRoute.passcode;
 
-    gottenRoute.event_position.event['category'] = eventCategory;
+  const eventCategory = gottenRoute.event_position.event.event_category;
+  delete eventCategory.id;
+  delete eventCategory.id_organization;
+  delete eventCategory.id_ref;
 
-    delete gottenRoute.event_position.event.event_category;
+  gottenRoute.event_position.event["category"] = eventCategory;
 
-    gottenRoute['event'] = gottenRoute.event_position.event;
-    delete gottenRoute['event'].id;
-    delete gottenRoute['event'].id_organization;
-    delete gottenRoute['event'].id_event_category;
-    delete gottenRoute['event'].id_ref;
+  delete gottenRoute.event_position.event.event_category;
 
-    delete gottenRoute.event_position;
+  gottenRoute["event"] = gottenRoute.event_position.event;
+  delete gottenRoute["event"].id;
+  delete gottenRoute["event"].id_organization;
+  delete gottenRoute["event"].id_event_category;
+  delete gottenRoute["event"].id_ref;
+
+  delete gottenRoute.event_position;
 }

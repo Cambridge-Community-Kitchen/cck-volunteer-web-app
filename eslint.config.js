a011eeb7e27926532493d97c5f85d4d28de8bae5
Daniel Perrett <perrettdl@googlemail.com>
2025-11-20 22:41:38 +0000

    fix: linting is now up to date

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintJs from "@eslint/js";
+import eslintTs from 'typescript-eslint';
+import nextPlugin from '@next/eslint-plugin-next'
+// import nextVitals from 'eslint-config-next/core-web-vitals'
+
+export default defineConfig([
+  eslintJs.configs.recommended,
+  eslintTs.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+    },
+  },
+  {
+    files: ['**/*.test.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      globals: {
+        describe: "readonly",
+        it: "readonly",
+        test: "readonly",
+        expect: "readonly",
+      },
+    },
+  },
+  // ...nextVitals,
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
